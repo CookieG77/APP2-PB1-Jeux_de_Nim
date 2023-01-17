ee0d734dd3ee8b06ec613e1b6006ab03bba4bdb3
CookieG77 <mcordonn9277@gmail.com>
2023-01-17 10:04:46 +0100

    class jeu

--- classes.py
+++ classes.py
@@ -1,3 +1,47 @@
 """
 fichier secondaire contenant les classes nécessaire au fonctionnement du jeu
 """
+
+class JdNim:
+    """
+    Classe permettant d'organiser les données du jeu de Nim.
+    """
+    def __init__(self,
+                 orga: list     
+    ) -> None:
+        """
+        Initialisation de la classe.
+        """
+        self.plateau = []
+        for i in orga:
+            temp = []
+            for j in range(i):
+                temp.append(j)
+            self.plateau.append([temp, i])
+
+    def load_save(self,
+                  save: list[str, list]
+    ) -> None:
+        """
+        Fonction permettant de charger une sauvegarde du jeu.
+        """
+        print(1)
+
+    def del_elements(self,
+                     selection: list
+    ) -> None:
+        """
+        Fonction permettant de supprimer une selection d'objet (une liste).
+        Chaques objet est représenté par un couple (x,y)
+        """
+        plateau_temp = self.plateau[:]
+        for i in selection:
+            ligne = plateau_temp[i[0]]
+            ligne[0].pop(i[1])
+            plateau_temp[i[0]] = ligne
+            
+
+
+test = JdNim([7,5,3,1])
+test.del_elements([(0,0),(1,0),(0,3)])
+print(test.plateau)
--- main.py
+++ main.py
@@ -3,3 +3,32 @@
 Fichier principale contenant l'execution du code permettant au lancement du jeux de Nim.
 """
 
+import json
+from fltk import cree_fenetre, ferme_fenetre, donne_ev, type_ev, touche, image, rectangle
+
+def load_json(filename: str, directory: str = "") -> dict:
+    """
+    Permet de charger le dico stocker en format json
+    """
+    with open(directory + filename, encoding="utf-8") as file:
+        content = json.load(file)
+    file.close()
+    return content
+
+
+def save_json(directory: str, filename: str, content: any):
+    """
+    Permet de sauvegarder le dico en format json
+    """
+    with open(directory + filename, "w", encoding="utf-8") as file:
+        json.dump(content, file)
+    file.close()
+
+def launch_game(gameconfig: dict) -> None:
+    """
+    Fonctions qui lance le jeu tout en prenant en compte si le jeu est sauvegarder.
+    """
+
+
+configjson = load_json("config.json")
+launch_game(configjson)
