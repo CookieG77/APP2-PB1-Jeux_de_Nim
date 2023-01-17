993211c44081cc3cb0b663d860f364c44ad23100
CookieG77 <mcordonn9277@gmail.com>
2023-01-17 08:42:33 +0100

    ajout texture alumette + docstring main

---
+++ main.py
@@ -0,0 +1,5 @@
+#All the code in this file was created by Maxime Cordonnier and Paul Amouroux for a school project.
+"""
+Fichier principale contenant l'execution du code permettant au lancement du jeux de Nim.
+"""
+
