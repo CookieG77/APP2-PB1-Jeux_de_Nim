31e0c082a9ea615c83158f423a304865f16326e3
DominataurusRex <95956715+DominataurusRex@users.noreply.github.com>
2023-01-19 12:08:36 +0100

    Update ButtonRect

--- mad_fltk.py
+++ mad_fltk.py
@@ -50,7 +50,7 @@ class ButtonRect:
         self.border = border
 
     def draw(self,
-             color_int: str,
+             color_int: str = "",
              color_ext: str = "#000000",
              text: str = "",
              font: str = "Helvetica"
@@ -67,7 +67,9 @@ class ButtonRect:
                          abs(self.coord_a[1] - self.coord_b[1])
                          )
             font_size = get_font_size(text, dimension, font)
-            texte(dimension[0] // 2, dimension[1] // 2,
+            placement = ((self.coord_a[0] + self.coord_b[0]) // 2,
+                         (self.coord_a[1] + self.coord_b[1]) // 2)
+            texte(placement[0], placement[1],
                   text, color_ext, "c", font,
                   font_size
                   )
@@ -85,8 +87,8 @@ class ButtonRect:
         gap_x = dim_overlay[0] // 2
         gap_y = dim_overlay[1] // 2
         coord_x, coord_y = abscisse_souris(), ordonnee_souris()
-        rectangle((coord_x - gap_x, coord_y - dim_overlay),
-                  (coord_x + gap_x, coord_y),
+        rectangle(coord_x - gap_x, coord_y - dim_overlay[1],
+                  coord_x + gap_x, coord_y,
                   color_ext,
                   color_int,
                   tag="overlay"
@@ -94,8 +96,9 @@ class ButtonRect:
         texte(coord_x,
               coord_y - gap_y,
               text,
+              color_ext,
               "c",
-              taille_texte=font_size,
+              taille=font_size,
               tag="overlay"
               )
 
